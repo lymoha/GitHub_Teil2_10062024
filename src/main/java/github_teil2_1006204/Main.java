50da77aeae7237deaeb6c7f759e154ba008cba76
Mohammed Ly <lybhoye@yahoo.com>
2024-06-10 12:00:41 +0200

    Initial Commit Reniew Commit

--- src/main/java/github_teil2_1006204/Main.java
+++ src/main/java/github_teil2_1006204/Main.java
@@ -3,5 +3,6 @@ package github_teil2_1006204;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Test me on github!!!");
     }
 }

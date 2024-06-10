d96546ee16ecd816d6337f06f82aabd814570c9a
Mohammed Ly <lybhoye@yahoo.com>
2024-06-10 12:12:15 +0200

    Initial Commit after the old commit!!!

--- src/main/java/github_teil2_1006204/Main.java
+++ src/main/java/github_teil2_1006204/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Test me on github!!!");
+        System.out.println("hello again and again!!!");
+        System.out.println("this just a test in pulling and pushing in github!!");
     }
 }
